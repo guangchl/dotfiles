f7b971e8ab8aab70c07f1d8a4ae434ce034e5dd9
Guangcheng Lu <ethanlugc@gmail.com>
2016-10-06 01:31:09 -0700

    Correct the vim build script with lua support.

--- vim/buildvim_mac.sh
+++ vim/buildvim_mac.sh
@@ -1,5 +1,13 @@
 # build vim with lua support
-make distclean                 # in case it is built before
-./configure --with-lua-prefix
+# brew install lua before running this script
+
+# in case it is built before
+make distclean
+
+# configure the build
+./configure --enable-luainterp \
+            --with-lua-prefix=/usr/local
+
+# build and install at /usr/local/bin/vim
 make
-sudo make install              # at /usr/local/bin/vim
+sudo make install
